9cafef63e55b3774db39e86186422383adb44c3b
Abdelrahman Amr <41464725+abdelrahmanamr6600@users.noreply.github.com>
2022-06-18 19:33:53 +0200

    some edit
    add request location Permission
    add icon in location activity

--- app/src/main/java/com/amrmedhatandroid/tahliluk_laboratory/activities/LocationActivity.kt
+++ app/src/main/java/com/amrmedhatandroid/tahliluk_laboratory/activities/LocationActivity.kt
@@ -1,13 +1,16 @@
 package com.amrmedhatandroid.tahliluk_laboratory.activities
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
 import com.amrmedhatandroid.tahliluk_laboratory.R
 import com.amrmedhatandroid.tahliluk_laboratory.databinding.ActivityLocationBinding
@@ -35,6 +38,7 @@ class LocationActivity : AppCompatActivity() {
         setContentView(mActivityLocationBinding.root)
         bindingDialog = DialogProgressBinding.inflate(layoutInflater)
         mCurrentLatLong = LatLng(0.0, 0.0)
+        checkPermission()
 
 
         mActivityLocationBinding.btnGetLocation.setOnClickListener {
@@ -83,6 +87,20 @@ class LocationActivity : AppCompatActivity() {
         onBackPressed()
 
 
+
+    }
+
+    private fun checkPermission(){
+        if ( ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION),101)
+        }
 
     }
 }
